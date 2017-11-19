585e35c5263aa7539d116c8d281c42ededfa74b1
Luminus Olumide Alabi <luminus@vedutanova.com>
2017-11-19 17:09:24 +0000

    Solve all the assignment questions

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,67 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |num|
+    sum += num
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if arr.empty?
+    0
+  elsif arr.length == 1
+    arr[0]
+  else
+    max_first = arr.sort.reverse!
+    sum = max_first[0] + max_first[1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length < 2 || !arr.combination(2).detect { |a, b| a + b == n }
+    false
+  else
+    true
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  true unless s.length < 1 || s[0].match(/^([aeiouAEIOU]|\W|\s)/)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.empty? || s[0].match(/^([aeiouAEIOU]|\W|\s)/)
+    false
+  elsif s.to_i(base=2) % 4 == 0
+    true
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    if !isbn.empty? && price > 0
+      @isbn = isbn
+      @price = price
+    else
+      raise ArgumentError
+    end
+  end
+
+  def price_as_string
+    "$#{format("%.2f", @price)}"
+  end
 end
